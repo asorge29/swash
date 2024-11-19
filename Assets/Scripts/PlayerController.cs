4a3ab27e90d510d0c584d83b3df813736d6206d9
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-11-19 12:30:06 -0800

    replace sand texture, work on playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [SelectionBase]
@@ -8,45 +6,38 @@ public class PlayerController : MonoBehaviour
     //TODO: fix the region and enum bullshit
     //TODO: vertical and horizontal walking animation
     //TODO: attack
-    //TODO: health
     //TODO: pickups
-    //TODO: Talk to archer
+    
+    public float moveSpeed = 250f;
+    public int maxHealth = 100;
 
+    public float attackCooldown = 0.2f;
+    public float attackRange = 0.5f;
+
+    public bool visible = true;
     
-    #region Enums
+    public bool dashAbility = false;
     
-    private enum Directions { UP, DOWN, LEFT, RIGHT }
-    
-    #endregion
-    
-    #region Editor Data
-    
-    [Header("Movement Attributes")]
-    [SerializeField] public float moveSpeed = 50f;
-    
-    [Header("Dependencies")]
     [SerializeField] Rigidbody2D _rb;
     [SerializeField] Animator _animator;
     [SerializeField] SpriteRenderer _spriteRenderer;
-    
-    #endregion
-    
-    #region Internal Data
+
+    [HideInInspector] public int health;
+
+    private bool _canDash;
+    private bool _dashCdown;
+    private bool _attackCdown;
     
     private Vector2 _moveDir = Vector2.zero;
-    private Directions _facingDirection = Directions.RIGHT;
     
     private readonly int _animMoveRight = Animator.StringToHash("anim_player_move_right");
     private readonly int _animIdleRight = Animator.StringToHash("anim_player_idle_right");
+
     
-    #endregion
-    
-    #region Tick
 
     private void Update()
     {
         GatherInput();
-        CalculateFacingDirection();
         UpdateAnimation();
     }
 
@@ -55,56 +46,30 @@ public class PlayerController : MonoBehaviour
         MovementUpdate();
     }
     
-    #endregion
-    
-    #region Input Logic
 
     private void GatherInput()
     {
         _moveDir.x = Input.GetAxisRaw("Horizontal");
         _moveDir.y = Input.GetAxisRaw("Vertical");
     }
-    
-    #endregion
-    
-    #region Movement Logic
 
     private void MovementUpdate()
     {
         _rb.velocity = _moveDir.normalized * moveSpeed * Time.fixedDeltaTime;
     }
     
-    #endregion
-    
-    #region Animation Logic
-
-    private void CalculateFacingDirection()
-    {
-        if (_moveDir.x != 0)
-        {
-            if (_moveDir.x > 0) // moving right
-            {
-                _facingDirection = Directions.RIGHT;
-            }
-            else if (_moveDir.x < 0) // moving left
-            {
-                _facingDirection = Directions.LEFT;
-            }
-        }
-    }
-
     private void UpdateAnimation()
     {
-        if (_facingDirection == Directions.LEFT)
+        if (_moveDir.x < 0)
         {
             _spriteRenderer.flipX = true;
         }
-        else if (_facingDirection == Directions.RIGHT)
+        else if (_moveDir.x > 0)
         {
             _spriteRenderer.flipX = false;
         }
 
-        if (_moveDir.sqrMagnitude > 0) // moving
+        if (_moveDir.sqrMagnitude > 0)
         {
             _animator.CrossFade(_animMoveRight, 0);
         }
@@ -114,6 +79,5 @@ public class PlayerController : MonoBehaviour
         }
     }
     
-    #endregion
     
 }
