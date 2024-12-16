0d44dafd35a8576f2d238569d8b856036c5cac30
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-12-16 12:35:44 -0800

    world manager script and object

---
+++ Assets/Scripts/WorldManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldManager : MonoBehaviour
+{
+    private EnemyAi[] enemies;
+    
+    void Start()
+    {
+        enemies = FindObjectsOfType<EnemyAi>();
+    }
+    
+    void FixedUpdate()
+    {
+        enemies = FindObjectsOfType<EnemyAi>();
+        
+        if (enemies.Length <= 0)
+        {
+            SceneManager.LoadScene("Scenes/Cutscenes/Cutscene 1/Intro Cutscene");
+        }
+    }
+
+    void SpawnBoat()
+    {
+        
+    }
+}
